186fa2c3bf594fa42d4452e97706d0c5dec20893
Fabricio Frontarolli <fabriciofrontarolli@Fabricios-Air.Dlink>
2024-02-08 14:24:16 -0300

    adding api link

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://138.197.227.33'
-: 'https://138.197.227.33';
+? 'https://infomatao.com'
+: 'https://infomatao.com';
 
 export default baseUrl;
